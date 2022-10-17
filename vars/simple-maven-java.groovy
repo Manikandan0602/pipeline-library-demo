f7ca4d34c46d2370f49e18dcec4cc19ef31e3f77
Manikandan0602 <pcmanienquiries@gmail.com>
2022-10-17 23:06:47 +0530

    updated java file

---
+++ vars/simple-maven-java.groovy
@@ -0,0 +1,63 @@
+#!/usr/bin/env groovy
+def call(){
+    pipeline{
+        agent {
+            label 'build_server'
+        } 
+        tools{
+            maven 'maven 3'
+        }
+        stages{
+            stage("build"){
+                steps{
+                    script{
+                        sh 'mvn install'
+                    }
+
+                }
+            }
+            stage("unit testing"){
+                steps{
+                    
+                    sh 'mvn test'
+                }
+                post{
+                    success{
+                        echo "junit testing is success,publishing report"
+                        junit 'target/surefire-reports/*.xml'
+                    
+                    }
+                    failure{
+                        echo "junit testing is failed"
+
+                    }
+                }
+            }
+            /*stage("sonar"){
+                steps{
+                    script{
+                        withSonarQubeEnv(credentialsId: 'my sonar') {
+                            sh "${tool("sonarqube")}/bin/sonar-scanner \
+                            -Dsonar.projectKey=simple-java-maven-app \
+                            -Dsonar.sources=. \
+                            -Dsonar.java.binaries=target \
+                            -Dsonar.host.url=http://172.31.15.244:9000 \
+                            -Dsonar.login=sqp_9114f8d385468d2dc10d5b6379ea32f41293698a"
+        
+                        }
+                    }
+                }
+
+            }*/
+            /*stage("upload artifact"){
+                steps{
+                sh 'mvn -s settings.xml deploy'
+                }
+            }*/
+            
+            
+
+        }
+
+    }
+}
